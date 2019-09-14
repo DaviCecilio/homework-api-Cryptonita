c1378e448a8751d07cc08f3816ea7d6d2d729504
DaviCecilio <davim.cecilio@gmail.com>
2019-09-14 16:26:13 -0300

    First NodeJS

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require("express");
 const app = express();
 const cors = require('cors')
 app.use(cors())
+
 //importo o arquivo que simula o banco de dados
-const { performance, username } = require("./database");
+const { performance, username, portfolio} = require("./database"); //Banco de Dados já setado!
 
 /**
  * Endpoit para testar se a API está funcionando
@@ -30,7 +31,8 @@ app.get("/performance/:usernameId", (req, res) => {
   //Verifica se foram encontrados usuários com o Id informado
   if (usernameExists.length <= 0)
     //retorna um json com a mensagem de erro
-    return res.status(404).json({ message: "USER_NOT_FOUND" });
+    return res.status(404)
+    .json({ message: "Não foi encontrado dados para este usuário" });
 
   //função que filtra as performances deixando apenas aquelas
   //que possuem o usernameId informado
@@ -47,8 +49,22 @@ app.get("/performance/:usernameId", (req, res) => {
  * requisição get passando o usernameId como parametro
  * exemplo da requisição: http://localhost:8080/portfolio/1
  */
+
 app.get("/portfolio/:usernameId", (req, res) => {
-/**TODO */
+  //extraio o usernameId do parametro
+  const { usernameId } = req.params;
+
+  //Função que filtra os usuários que possuem o usernameId informado
+  const portfolioUser = portfolio.filter(
+    p => p.usernameId === parseInt(usernameId)
+  );
+
+  if (portfolioUser.length === 0) {
+    return res
+      .status(404)
+      .json({ message: "Não foi encontrado dados para este usuário" });
+  }
+  return res.status(200).json(portfolioUser);
 });
 
 app.listen(process.env.PORT || 8080);
--- database.js
+++ database.js
@@ -1,19 +1,27 @@
 const moment = require("moment");
 
 exports.username = [
-  { usernameId: 1, name: "Satoshi Nakamoto" },
-  { usernameId: 2, name: "Vitalik Buterin" }
+  {
+    usernameId: 1,
+    name: "Satoshi Nakamoto"
+  },
+  {
+    usernameId: 2,
+    name: "Vitalik Buterin"
+  }
 ];
 
 exports.performance = [
   {
+    // Informações referentes a Satoshi
     performanceId: 1,
     usernameId: 1,
     date: moment()
       .subtract(1, "days")
       .format("DD/MM/YYYY"),
-    performance: Math.random()
+    performance: 3
   },
+
   {
     performanceId: 2,
     usernameId: 1,
@@ -22,6 +30,7 @@ exports.performance = [
       .format("DD/MM/YYYY"),
     performance: Math.random()
   },
+
   {
     performanceId: 3,
     usernameId: 1,
@@ -30,6 +39,7 @@ exports.performance = [
       .format("DD/MM/YYYY"),
     performance: Math.random()
   },
+
   {
     performanceId: 4,
     usernameId: 1,
@@ -38,6 +48,7 @@ exports.performance = [
       .format("DD/MM/YYYY"),
     performance: Math.random()
   },
+
   {
     performanceId: 5,
     usernameId: 1,
@@ -45,32 +56,129 @@ exports.performance = [
       .subtract(5, "days")
       .format("DD/MM/YYYY"),
     performance: Math.random()
+  },
+
+  {
+    performanceId: 6,
+    usernameId: 1,
+    date: moment()
+      .subtract(6, "days")
+      .format("DD/MM/YYYY"),
+    performance: Math.random()
+  },
+
+  //Informações referentes ao Vitalik
+  {
+    performanceId: 1,
+    usernameId: 2,
+    date: moment()
+      .subtract(1, "days")
+      .format("DD/MM/YYYY"),
+    performance: 2
+  },
+
+  {
+    performanceId: 2,
+    usernameId: 2,
+    date: moment()
+      .subtract(2, "days")
+      .format("DD/MM/YYYY"),
+    performance: Math.random()
+  },
+
+  {
+    performanceId: 3,
+    usernameId: 2,
+    date: moment()
+      .subtract(3, "days")
+      .format("DD/MM/YYYY"),
+    performance: Math.random()
+  },
+
+  {
+    performanceId: 4,
+    usernameId: 2,
+    date: moment()
+      .subtract(4, "days")
+      .format("DD/MM/YYYY"),
+    performance: Math.random()
+  },
+
+  {
+    performanceId: 5,
+    usernameId: 2,
+    date: moment()
+      .subtract(5, "days")
+      .format("DD/MM/YYYY"),
+    performance: Math.random()
+  },
+
+  {
+    performanceId: 6,
+    usernameId: 2,
+    date: moment()
+      .subtract(6, "days")
+      .format("DD/MM/YYYY"),
+    performance: Math.random()
   }
 ];
 
 exports.portfolio = [
-    {
-        portfolioId: 1,
-        usernameId: 1,
-        coin: 'Bitcoin',
-        amount: 1
-    },
-    {
-        portfolioId: 2,
-        usernameId: 1,
-        coin: 'Ethereum',
-        amount: 1.5
-    },
-    {
-        portfolioId: 3,
-        usernameId: 1,
-        coin: 'Dogecoin',
-        amount: 0.5
-    },
-    {
-        portfolioId: 4,
-        usernameId: 1,
-        coin: 'Decred',
-        amount: 2
-    }
+  //Portólio referente a Satoshi
+  {
+    portfolioId: 1,
+    usernameId: 1,
+    coin: "Bitcoin",
+    amount: 1
+  },
+
+  {
+    portfolioId: 2,
+    usernameId: 1,
+    coin: "Ethereum",
+    amount: 1.5
+  },
+
+  {
+    portfolioId: 3,
+    usernameId: 1,
+    coin: "Dogecoin",
+    amount: 0.5
+  },
+
+  {
+    portfolioId: 4,
+    usernameId: 1,
+    coin: "Decred",
+    amount: 2
+  },
+
+  //Portólio referente ao Vitalik
+  {
+    portfolioId: 1,
+    usernameId: 2,
+    coin: "IOTA",
+    amount: 2
+  },
+
+  {
+    portfolioId: 2,
+    usernameId: 2,
+    coin: "Tether",
+    amount: 3.9
+  },
+
+  {
+    portfolioId: 3,
+    usernameId: 2,
+    coin: "EOS",
+    amount: 0.6
+  },
+
+  {
+    portfolioId: 4,
+    usernameId: 2,
+    coin: "Stellar",
+    amount: 5
+  }
 ];
